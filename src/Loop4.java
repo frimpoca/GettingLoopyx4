bbd4502cdb8a3b9031364b3cd14f4d83d6becf14
frimpoca <frimpoca@mail.uc.edu>
2023-10-26 18:42:51 -0400

    x4

---
+++ src/Loop4.java
@@ -0,0 +1,7 @@
+public class Loop4 { // Begin class
+    public static void main(String [] args){ // start main()
+        for (int i=10; i>=0; i=i-2){ // decrement the loop counter
+            System.out.println(i); // print i
+        } // end of for loop
+    } // end of main()
+} // End of class
